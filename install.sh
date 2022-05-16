5a4ef5df53c88bbd412f10c4b3f5ae028a97e6c7
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-05-16 04:07:24 -0500

    Installation script updated

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cargo build --release
 # Make sure to run this command with sudo.
 sudo mkdir /usr/share/eunomia
 sudo cp -r target/release/eunomia /usr/share/eunomia
-sudo cp -r Audio/ /usr/share/eunomia
 
-chmod +x eunomia
+cd /usr/share/eunomia
+sudo chmod +x eunomia
 sudo cp eunomia /usr/bin/
